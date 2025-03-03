b16cfbc7a302990f1bdaef3ee78c4738c5d71b6a
VaclavDalibaba <vasekdalibaba@gmail.com>
2025-03-03 11:23:32 +0100

    Implementace funkce na vypsání textu do konzole i na displej

--- lekce_2/du2_template.py
+++ lekce_2/du2_template.py
@@ -2,6 +2,5 @@ from microbit import display
 
 class Obrazovka:
     def vypis(text):
-        # zde implementujte tuto funkci tak,
-        # aby vstupni "text" vypsala jak na microbitu,
-        # tak v terminalu na pocitaci
+        print(text)
+        display.scroll(text)
