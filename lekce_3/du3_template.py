48b565e769c35dbfb2a77a5516453979bb496f60
VaclavDalibaba <vasekdalibaba@gmail.com>
2025-03-12 15:39:53 +0100

    Oprava - Implementace funkce pro voládání motorů

--- lekce_3/du3_template.py
+++ lekce_3/du3_template.py
@@ -6,22 +6,32 @@ def init_motoru():
     sleep(100)
 
 def jed(strana, smer, rychlost):
-    if strana == "leva" and smer == "dopredu":
-        x = b'\x05'
-    elif strana == "leva" and smer == "dozadu":
-        x = b'\x04'
-    elif strana == "prava" and smer == "dopredu":
-        x = b'\x03'
-    elif strana == "prava" and smer == "dozadu":
-        x = b'\x02'
+    if rychlost >= 0 and rychlost <= 250:
+        if strana == "leva" and smer == "dopredu":
+            x = b'\x05'
+        elif strana == "leva" and smer == "dozadu":
+            x = b'\x04'
+        elif strana == "prava" and smer == "dopredu":
+            x = b'\x03'
+        elif strana == "prava" and smer == "dozadu":
+            x = b'\x02'
+        else:
+            x = "fail"
+
+        if x == "fail":
+            print("Spatne parametry")
+        else:
+            i2c.write(0x70, x + bytes([rychlost]))
     else:
         print("Spatne parametry")
-    
-    i2c.write(0x70, x + bytes([rychlost]))
+
 
 if __name__ == "__main__":
     i2c.init()
     init_motoru()
+    jed("leva", "dozadu", 0)
+    jed("prava", "dozadu", 0)
+    sleep(1000)
     jed("leva", "dopredu", 135)
     jed("prava", "dopredu", 135)
     sleep(1000)
